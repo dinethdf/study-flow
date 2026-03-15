45665967a68c23f3bd02ed641a92ccc8a08e9e98
Dineth Dilshan <116701541+dinethdf@users.noreply.github.com>
2026-03-15 16:28:09 +0530

    feat: initial commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,6 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
-
-export function formatDate(date: Date | string) {
-    return new Intl.DateTimeFormat('en-US', {
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-    }).format(new Date(date));
-}
-
-export function formatTime(date: Date | string) {
-    return new Intl.DateTimeFormat('en-US', {
-        hour: 'numeric',
-        minute: '2-digit',
-        hour12: true,
-    }).format(new Date(date));
+  return twMerge(clsx(inputs))
 }
